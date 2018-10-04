e1efd75dd42b035a23096d2c451ec16fc02b676d
monado3 <monado.kk@gmail.com>
2018-10-04 05:42:48 +0000

    I tried 4.2.7, but it didn't work.And, delete the line 'dl_exp_nlp' in gitignore

--- dl_exp_cv/dataset.py
+++ dl_exp_cv/dataset.py
@@ -1,6 +1,11 @@
+import os.path as op
+from glob import glob
+
 import chainer
 import numpy as np
 from PIL import Image
+
+MINI_CIFAR_DIR = op.join(op.dirname(op.abspath(__file__)), 'mini_cifar')
 
 
 class MyCifarDataset(chainer.dataset.DatasetMixin):
@@ -19,6 +24,7 @@ class MyCifarDataset(chainer.dataset.DatasetMixin):
             'ship': 8,
             'truck': 9
         }
+        self._paths = glob(op.join(MINI_CIFAR_DIR, train_dir, '*'), recursive=True)
 
     def __len__(self):
         """ データセットの数を返す関数 """
@@ -28,14 +34,14 @@ class MyCifarDataset(chainer.dataset.DatasetMixin):
         """    chainerに入力するための画像ファイルと，そのラベルを出力する．"""
 
         # TODO self._pathsからi番目のpathを取得して画像として読み込む
-        path = ''
+        path = self._paths[i]
         image = np.array(Image.open(path).convert('RGB').resize((224, 224)), np.float32)
         mean = [123.68, 116.779, 103.939]
         image -= mean
 
         # 画像データは3次元配列で入っており，その軸は(幅，高さ，チャンネル)の順である
         # TODO chainerは(チャンネル，幅，高さ)で入力することを求めるのでtransposeを行なう
-        image_for_chainer = image.transpose(('?', '?', '?'))
+        image_for_chainer = image.transpose((2, 1, 0))
 
         # pathから画像のラベルを取得することが出来る．
         # ラベルはairplaneのような文字列型ではなく，0のような整数である必要がある．
@@ -44,4 +50,4 @@ class MyCifarDataset(chainer.dataset.DatasetMixin):
 
     def get_label_from_path(self, path):
         # TODO pathからlabel(0~9)を推測して返す関数を実装せよ
-        raise NotImplementedError
+        return self._labels[op.dirname(path)]
